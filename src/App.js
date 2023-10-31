ba2cc9ab325defa89f8a50b84a2047bbbc4c34d2
fadoua <fadouadahech93@gmail.com>
2023-10-31 15:27:31 +0100

    up dating

--- src/App.js
+++ src/App.js
@@ -3,21 +3,24 @@ import React, { Component } from "react";
 class App extends Component {
   state = {
     person: {
-      fullName: "mohamed ben ali",
-      bio: "A surgeon is a medical professional who specializes in performing surgeries to treat various medical conditions.",
+      fullName: "Mohamed Ben Ali",
+      bio: "Un chirurgien est un professionnel de la médecine spécialisé dans la réalisation de chirurgies pour traiter diverses affections médicales.",
       imgSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxaCOQKSHIK4XDJiD5aD3ilsT17hkmRf_QDw&usqp=CAU",
-      profession: "surgeon",
+      profession: "chirurgien",
     },
-    shows: false,
-    intervalId: null,
-    secondsSinceMount: 0,
+    shows: false, // Un état pour afficher ou masquer les détails de la personne
+    intervalId: null, // Un identifiant pour gérer l'intervalle de mise à jour du temps depuis le montage du composant
+    secondsSinceMount: 0, // Le nombre de secondes depuis le montage du composant
   };
 
+  // Méthode pour basculer l'affichage des détails de la personne
   toggleShow = () => {
     this.setState((prevState) => ({ shows: !prevState.shows }));
   };
 
+  // Méthode appelée après le montage du composant
   componentDidMount() {
+    // Met en place un intervalle pour mettre à jour le temps depuis le montage du composant
     const intervalId = setInterval(() => {
       this.setState((prevState) => ({
         secondsSinceMount: prevState.secondsSinceMount + 1,
@@ -27,7 +30,9 @@ class App extends Component {
     this.setState({ intervalId });
   }
 
+  // Méthode appelée avant la suppression du composant
   componentWillUnmount() {
+    // Nettoie l'intervalle pour éviter les fuites de mémoire
     clearInterval(this.state.intervalId);
   }
 
@@ -42,10 +47,10 @@ class App extends Component {
             <img src={person.imgSrc} alt={person.fullName} />
             <h1>{person.fullName}</h1>
             <p>{person.bio}</p>
-            <p>Profession: {person.profession}</p>
+            <p>Profession : {person.profession}</p>
           </div>
         )}
-        <p>Time since mount: {secondsSinceMount} seconds</p>
+        <p>Temps depuis le montage : {secondsSinceMount} secondes</p>
       </div>
     );
   }
